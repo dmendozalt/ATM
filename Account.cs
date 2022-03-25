a00d881f85315134a653fba9058fbcc214971c43
dmendozalt <dmendoza@lean-tech.io>
2022-03-24 20:50:59 -0500

    feat: add account model

---
+++ Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Account
+    {
+        public ulong Balance { get; set; }
+        public string AccountType { get; set; } //Savings,Checking
+
+        public Account(ulong initialBalance, string accountType)
+        {
+            Balance = initialBalance;
+            AccountType = accountType;
+        }
+
+        public ulong Deposit(ulong amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+        public ulong Withdraw(ulong amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+
+    }
+}
